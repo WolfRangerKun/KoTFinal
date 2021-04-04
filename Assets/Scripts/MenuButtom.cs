c89dc49a414040a3b3de604d7672917df05fe048
WolfRangerKun <isabofu@hotmail.com>
2021-04-03 23:45:17 -0300

    Menu

---
+++ Assets/Scripts/MenuButtom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtom : MonoBehaviour
+{
+    public void FindAMatch()
+    {
+        SceneManager.LoadScene("SceneKoT");
+    }
+}
--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -21,4 +21,5 @@ public class Win : MonoBehaviour
     {
         SceneManager.LoadScene("SceneKoT");
     }
+
 }
